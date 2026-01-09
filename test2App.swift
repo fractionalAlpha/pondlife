579719a64709c6ebc6450cb3e8a5307a7567ce06
fractionalAlpha <161514560+fractionalAlpha@users.noreply.github.com>
2026-01-09 01:12:56 -0700

    Add files via upload

---
+++ test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Thomas Holeva on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
